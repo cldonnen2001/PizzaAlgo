43c64154d4bd23607d42dee33a18e5e000ba2cf3
Christine Godfrey <cldonnen2001@yahoo.com>
2021-02-01 20:04:02 -0600

    added name request

---
+++ PizzaAlgo branch 1 a/PizzaAlgo branch 1 a/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PizzaAlgo_branch_1_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hey, {name}!");
+        }
+    }
+}
